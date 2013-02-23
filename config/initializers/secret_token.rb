2ef491179ad735003f04f69da15187e6a4c02385
ColbyAley <colby@aley.me>
2013-02-22 22:10:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imagenes::Application.config.secret_token = '8ff3e602117c5ae22c82df5be714aa8bb6295b68f9be0281b5ed804c46307922734d6f744609c1edc91d8ad1858704994d8cb84343cc9b19d90828a3bb641507'
